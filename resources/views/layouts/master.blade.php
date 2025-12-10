363b221faa6ef21a5a7f66abf5b155b06dd7b723
Diska Puspita Sari <diskapuspitasr@gmail.com>
2025-12-10 14:54:04 +0700

    add logo on head

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
     <title>@yield('title', 'Portfolio App')</title>
 
